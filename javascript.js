0aa20205c82657f15f2fa6c0ea014727a7d8a3d7
Jesse Chou <jessechou7@gmail.com>
2022-05-13 21:28:47 -0500

    Added computerPlay function

---
+++ javascript.js
@@ -0,0 +1,7 @@
+//Create function
+//Function randomly returns 'Rock', 'Paper', or 'Scissors'
+//The function is used to make the computer play
+
+let myWords = ['Rock', 'Papers', 'Scissors'];
+
+let computerPlay = myWords[Math.floor(Math.random() * myWords.length)];
